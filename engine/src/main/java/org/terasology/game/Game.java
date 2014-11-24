8de4626a1bb01bd5bd4c5afa271ecbf59e1b70e5
Rasmus Praestholm <cervator@gmail.com>
2014-11-23 21:19:45 -0500

    This is a compile error. Yay!

--- engine/src/main/java/org/terasology/game/Game.java
+++ engine/src/main/java/org/terasology/game/Game.java
@@ -45,6 +45,7 @@ public class Game {
     private static final Logger logger = LoggerFactory.getLogger(Game.class);
 
     private EngineTime time;
+    private Foo foo;
 
     private String name = "";
     private String seed = "";
